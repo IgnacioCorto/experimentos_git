893e3b6354272786f775c2b7653e659b7c5a0815
Ignacio Cortorreal <luis3ignacio@hotmail.com>
2014-02-09 05:30:06 +0000

    amortizacion: CF posee formula original

--- amortizacion/formulas.php
+++ amortizacion/formulas.php
@@ -9,14 +9,18 @@ $C = $prestamo;
 $i = $interes/$multiplo/100;
 $n = $anos*$multiplo;
 
-//$CF =  $C * pow(1 + $i, $n) ; 
-//$CF =  $C * $i / ( 100 * ( 1 - pow(1+$i/100, -$n) ) );
-
-$i_pow = pow(1+$i,$n);  $CF = $C * ($i*$i_pow) / ($i_pow-1);
+$i_pow = pow(1+$i,$n);
 
 $cuota = 3522.16;
 $artefacto = ($i*$i_pow) / ($i_pow-1);
 $capital_total = $cuota / $artefacto;
+
+//$CF =  $C * pow(1 + $i, $n) ; 
+//$CF =  $C * $i / ( 100 * ( 1 - pow(1+$i/100, -$n) ) );
+//$CF = $C * ($i*$i_pow) / ($i_pow-1);
+$CF =  $C * $i / (1 - pow(1+$i, -$n)) ;
+
+
 
 printf("%s ; %s\nTotal:%s\n",
        $CF, $CF*31, $capital_total);
